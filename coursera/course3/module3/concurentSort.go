61152d8b6a9c41a62b5f61efd91d3c4ed1981770
pchp <pascal.chenevas-paule@gmx.de>
2019-10-26 20:30:02 +0200

    add function to convert []string to []int and alog to devide a slice into 4 slices

--- coursera/course3/module3/concurentSort.go
+++ coursera/course3/module3/concurentSort.go
@@ -1,43 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	sli := []int{10, 33, 5, -1, 55, 12, 13, 45, 9, 14, 534, 78, 40, -5, -6, -20, 0, 55, 60}
+func StringSliceToStringInt(sli []string) []int {
 
-	var slices [4][]int
+	sliInt := make([]int, len(sli))
+	for i := 0; i < len(sli); i++ {
+
+		s, err := strconv.Atoi(sli[i])
+
+		if err == nil {
+			sliInt[i] = s
+		}
+	}
+	return sliInt
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	numberArray := 4
+	var slices [][]int
 	result := []int{}
 
-	fmt.Println(sli)
 	ch := make(chan []int)
 
-	idx := 1
-	slices[0] = sli[:4]
-	for i := 4; i <= len(sli); i++ {
-		if i%4 == 0 && i+4 < len(sli) {
-			slices[idx] = sli[i : i+4]
-			idx++
+	fmt.Print("> (Enter a list of integer): ")
+	input, _ := reader.ReadString('\n')
+	input = strings.Trim(input, "\n")
+	input = strings.ToLower(input)
+
+	sli := StringSliceToStringInt(strings.Split(input, " "))
+
+	//fmt.Println(sli)
+	chunkSize := (len(sli) + numberArray - 1) / numberArray
+
+	for i := 0; i < len(sli); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(sli) {
+			end = len(sli)
 		}
 
+		slices = append(slices, sli[i:end])
 	}
 
-	fmt.Println(slices)
+	//fmt.Println(slices)
 	for i := 0; i < len(slices); i++ {
 		go func(sli []int, i int) {
 			sort.Sort(sort.IntSlice(sli))
-			fmt.Println("slice #:", i, sli)
+			fmt.Println("sorted subarray #:", i, sli)
 			ch <- sli
 		}(slices[i], i)
 		result = append(result, <-ch...)
 	}
 
-	fmt.Println("====== result =====")
+	fmt.Println("====== result (sorted array) =====")
 	sort.Sort(sort.IntSlice(result))
 	fmt.Println(result)
-	fmt.Println(len(result))
-	fmt.Println(len(sli))
-
 }
